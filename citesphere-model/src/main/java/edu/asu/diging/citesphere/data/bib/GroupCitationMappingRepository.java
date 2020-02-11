2d7748d48aa7b42aecec617c8d038c90d2fb1cdc
Julia Damerow <julia.damerow@gmail.com>
2020-02-11 15:37:57 -0500

    [CITE-79] added more repository methods

--- citesphere-model/src/main/java/edu/asu/diging/citesphere/data/bib/GroupCitationMappingRepository.java
+++ citesphere-model/src/main/java/edu/asu/diging/citesphere/data/bib/GroupCitationMappingRepository.java
@@ -1,5 +1,8 @@
 package edu.asu.diging.citesphere.data.bib;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import edu.asu.diging.citesphere.model.bib.impl.CitationGroup;
@@ -8,5 +11,9 @@ import edu.asu.diging.citesphere.model.bib.impl.GroupCitationMapping;
 public interface GroupCitationMappingRepository
         extends PagingAndSortingRepository<GroupCitationMapping, String> {
 
+    List<GroupCitationMapping> findByGroup(CitationGroup group, Pageable pageable);
+    
+    long countByGroup(CitationGroup group);
+    
     void deleteByGroup(CitationGroup group);
 }
