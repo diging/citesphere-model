401229b2d3058538cdccaefa455f70231c864a40
Maulik Vajubhai Limbadiya (Student) <mlimbadi@sundevils.asu.edu>
2021-06-30 12:50:22 -0700

    [CITE-127] - Moved custom deletion repositories from citesphere to citesphere-model

---
+++ citesphere-model/src/main/java/edu/asu/diging/citesphere/data/bib/CustomCitationGroupRepository.java
@@ -0,0 +1,12 @@
+package edu.asu.diging.citesphere.data.bib;
+
+import org.bson.types.ObjectId;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import edu.asu.diging.citesphere.model.bib.impl.CitationGroup;
+
+@JaversSpringDataAuditable
+public interface CustomCitationGroupRepository extends MongoRepository<CitationGroup, ObjectId>{
+    Long deleteByGroupId(int groupId);
+}
